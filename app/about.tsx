cb8f0b2b011e3d78e3059ca7c109fa8ad3d8a3f8
Shourov Hasan <shourovhasancse@gmail.com>
2025-10-14 02:43:46 +0600

    create my first mobile app using react native

---
+++ app/about.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const about = () => {
+  return (
+    <View>
+      <Text className='text-4xl text-center text-accent'>about</Text>
+    </View>
+  )
+}
+
+export default about
+
+const styles = StyleSheet.create({})
---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl text-dark-200">Welcome to the Cinemaster</Text>
+      <Text className="text-2xl text-accent">Welcome to the world</Text>
+      <Link href="/about"> About</Link>
+    </View>
+  );
+}
